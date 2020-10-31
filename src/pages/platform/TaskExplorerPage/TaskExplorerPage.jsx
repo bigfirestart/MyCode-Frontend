17061eaa6d6177c75adc8e1cae9e7b45d32580da
Mark Karshtain <bigfirestart@gmail.com>
2020-10-31 12:58:32 +0300

    feat(tasks) tasks basic page

--- src/pages/platform/TaskExplorerPage/TaskExplorerPage.jsx
+++ src/pages/platform/TaskExplorerPage/TaskExplorerPage.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import {Card, Col, Container, Row} from "react-bootstrap";
 import TaskExplorerCard from "./TaskExplorerCard";
-import { getTasksList } from "../../../remote/api"
+import {getTasksList} from "../../../remote/api"
 
 export class TaskExplorerPage extends React.Component {
-    constructor(props) {
-        super(props);
-        getTasksList().then(console.log)
+    state = {
+        groupTasks: []
     }
+
+    componentDidMount() {
+        getTasksList().then(
+            data => this.setState({groupTasks: data})
+        )
+    }
+
+
+    taskSet = (groupTask, i) => {
+        return <TaskExplorerCard
+            groupId={groupTask.groupId}
+            taskId={groupTask.task.id}
+            taskName={groupTask.task.name}
+        />
+    }
+
+
     render() {
         return <Container fluid>
             <Row className="mt-5">
@@ -15,10 +31,9 @@ export class TaskExplorerPage extends React.Component {
                     <h2>Задачи</h2>
                 </Col>
             </Row>
-            <TaskExplorerCard taskName="Task 1" />
-            <TaskExplorerCard taskName="Task 1" />
-            <TaskExplorerCard taskName="Task 1" />
-
+            {
+                this.state.groupTasks.map(this.taskSet)
+            }
         </Container>
     }
 }
